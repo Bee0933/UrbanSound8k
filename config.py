82ed23be1fb72b1cf1074e52114c6340c9d25454
“Best <bestnyah7@gmail.com>
2022-06-03 22:48:39 +0100

    first ML process
    - EDA
    - feature engineering & selection
    - utility and preprocessing functions added
    - Two seperate datasets formed from feature extraction

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# file paths
+CSV_FILE_PATH = 'csv/UrbanSound8K.csv'
+AUDIO_FILES_PATH = 'UrbanSound8K/audio'
+SAVE_PATH = 'dataset/'
+EXTRACTED_FEATURE_SAVE_PATH = 'csv/extracted-features.csv'
+EXTRACTED_MFCC_FEATURE_SAVE_PATH = 'csv/extracted-MFCC-features.csv'
+
+
+# parameters
+N_DIMS = 40
