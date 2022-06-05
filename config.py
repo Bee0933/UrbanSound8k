59340d3ffa1a6ff8b50bfb720c79c492658bda41
“Best <bestnyah7@gmail.com>
2022-06-05 22:06:24 +0100

    picked files added

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 CSV_FILE_PATH = 'csv/UrbanSound8K.csv'
 AUDIO_FILES_PATH = 'UrbanSound8K/audio'
 SAVE_PATH = 'dataset/'
+PICKLE_SAVE_PATHS = 'pickle_data/'
 EXTRACTED_FEATURE_SAVE_PATH = 'csv/extracted-features.csv'
 EXTRACTED_MFCC_FEATURE_SAVE_PATH = 'csv/extracted-MFCC-features.csv'
 
